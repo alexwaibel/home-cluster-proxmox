6320eb7689eb14d29206aa253b319eb5dc0c7ed0
Alex Waibel <alexwaibel@users.noreply.github.com>
2021-04-15 19:23:51 -0700

    Provision k3os vm with terraform
    Some strange behavior here, namely that the VM needs manually rebooted
    otherwise the network gets borked and it fails to fetch my ssh key. Will
    work around this with ansible and k3os.install.power_off flag for now.

---
+++ server/terraform/proxmox/k3os.tf
@@ -0,0 +1,23 @@
+resource "proxmox_vm_qemu" "k3os" {
+    name = "k3os"
+    target_node="server"
+
+    iso = "local:iso/k3os-custom.iso"
+
+    cores = 2
+    memory = 8192
+    agent = 1
+
+    scsihw = "virtio-scsi-pci"
+
+    disk {
+        size = "50G"
+        type = "scsi"
+        storage = "local-zfs"
+    }
+
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+    }
+}
